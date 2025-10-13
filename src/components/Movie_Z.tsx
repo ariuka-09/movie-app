422e1b5bf8206afbc246598d4b480a8762951665
ariuka-09 <ariuntuguldur3@gmail.com>
2025-10-13 15:51:27 +0800

    test

--- src/components/Movie_Z.tsx
+++ src/components/Movie_Z.tsx
@@ -28,7 +28,7 @@ export function Movie_Z() {
           strokeLinejoin="round"
         />
       </svg>
-      <p className="text-[#4338CA] text-[16px] italic font-[700] ">Movie Zjhgfxdghkjhgfxdghgh</p>
+      <p className="text-[#4338CA] text-[16px] italic font-[700] ">Movie Z</p>
     </div>
   );
 }
